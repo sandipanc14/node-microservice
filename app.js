ff10dcae137bb13899a9ee7c01b1671e8b9dbfc3
sandipanc14 <119667977+sandipanc14@users.noreply.github.com>
2022-12-05 13:55:52 +0530

    1 file modified

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
 app.use(cors());
-// app.use(helmet());
+app.use(helmet());
 
 app.use('/', indexRouter);
 app.use('/items', itemsRouter);
